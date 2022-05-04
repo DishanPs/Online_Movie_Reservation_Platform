dcbb7022ac394f6df442845b0da10fb046dd2a9f
Dishancp <it20012106@my.sliit.lk>
2022-05-04 16:35:57 +0530

    Carousel Added

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+
+const CarouselX = () => {
+  return (
+      <div>
+          <Carousel>
+        <Carousel.Item interval={1000}>
+            <img
+            className="d-block w-100"
+            src="../images/avengers.jpg"
+            alt="First slide"
+            />
+            <Carousel.Caption>
+            <h3>Avengers</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="../images/645154.webp"
+            alt="Second slide"
+            />
+
+            <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="../images/inception.jpg"
+            alt="Third slide"
+            />
+
+            <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
+      </div>
+    
+  )
+}
+
+export default CarouselX
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,50 +2,15 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import './Homestyle.css'
-import Carousel from 'react-bootstrap/Carousel'
+import CarouselX from './Carousel'
+
 
 const Home = () => {
   return (
     <div>
         <Header/>
 
-        <Carousel>
-        <Carousel.Item interval={1000}>
-            <img
-            className="d-block w-100"
-            src="../images/avengers.jpg"
-            alt="First slide"
-            />
-            <Carousel.Caption>
-            <h3>Avengers</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src="../images/645154.webp"
-            alt="Second slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src="../images/inception.jpg"
-            alt="Third slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        </Carousel>
+        <CarouselX />
 
         <Footer/>
     </div>
