1d4236c33185815dcf4a12fd877f425ccc46b45d
Dishancp <it20012106@my.sliit.lk>
2022-05-04 15:58:03 +0530

    Header and Footer Added

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import './Homestyle.css'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
